f1a1e890d859b5ea4f4a24d7ae7406cd2c757e9d
Eman Shedeed <eman.shedid@jumia.com>
2023-06-16 06:18:45 +0300

    Remove storyboard and organize files

---
+++ CoreAnimationFun/CoreAnimationFun/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CoreAnimationFun
+//
+//  Created by Eman Shedeed on 16/06/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
